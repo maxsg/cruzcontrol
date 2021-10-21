927e5b07f34df900d1efa89e775d7410b4245a6b
Max Stein-Golenbock <max@barsala.com>
2021-10-21 01:52:44 -0700

    Add gitignore. Add waitress config instead of gunicorn and wsgi.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from services.twitter.twitter import get_tweets
+from .services.twitter.twitter import get_tweets
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -7,3 +8,7 @@ app = Flask(__name__)
 def home():
     tweets = get_tweets()
     return tweets
+
+if __name__ == "__main__":
+    #We now use this syntax to server our app. 
+    serve(app, host='0.0.0.0', port=5000)
